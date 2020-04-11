f919ccb07f046dd1c9390ba9994fb76bec9208b4
Sean Toru <sean@toruinteractive.com>
2020-04-11 10:08:45 +0100

    Process postcodes

--- process-raw-postcodes.js
+++ process-raw-postcodes.js
@@ -4,8 +4,16 @@ const neatCsv = require ('neat-csv')
 const fs = require ('fs')
 
 const processCsvFile = data => {
-  console.log (data)
+  const postcodes = data
+                      .map (r => r.address_personal_postcode)
+                      .filter (r => r != "" && r != undefined)
+                      .map (r => r.toUpperCase ())
+                      .map (r => r.replace(/ /g,''))
+                      .map (r => r.slice (0,-3) + " " + r.slice (r.length - 3))
+
+  console.log (postcodes)
 }
+
 
 fs.readFile('data/raw.csv', async (err, data) => {
   if (err) {
